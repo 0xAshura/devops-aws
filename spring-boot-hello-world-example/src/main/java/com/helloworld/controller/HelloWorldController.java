b1cf2c3957402fea429ab4d7794d8ab5188c8b30
Mihir Limbad <mihir.limbad@dcmail.ca>
2025-02-05 14:19:42 -0500

    Resolved merge conflict - 3 in HelloWorldController.java

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController 
 {
 @RequestMapping("/")
-public String hello() {
+public String hello() 
+{
 return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
 }
 }
-
-
